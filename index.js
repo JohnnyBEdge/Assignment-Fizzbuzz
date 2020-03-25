d737de1402c68267d3999fa16956755e9fce6125
John Martin <jrmartin182@gmail.com>
2020-03-25 11:31:25 -0600

    completed assignment with three loops

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 'use strict';
 
 //While loop
-// function usingWhile(){
-//     let i = 1;
-//     while(i <= 100){
-//         if(i % 3 === 0 && i % 5 === 0){
-//             console.log("FizzBuzz");
-//         } else if(i % 3 === 0){
-//             console.log("Fizz");
-//         } else if (i % 5 === 0){
-//             console.log("Buzz");
-//         }
-//         i++
-//     }
-// }
-// usingWhile()
+function usingWhile(){
+    let i = 1;
+    while(i <= 100){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log("Fizz");
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+        i++
+    }
+}
+
 
 //Do...While loop
-// function usingDoWhile(){
-//     let i = 1;
-//     do {
-//         if(i % 3 === 0 && i % 5 === 0){
-//             console.log("FizzBuzz");
-//             } else if(i % 3 === 0){
-//                 console.log("Fizz");
-//             } else if (i % 5 === 0){
-//                 console.log("Buzz");
-//             }
-//         i++
-//     } while (i <= 100);
-// }
-// usingDoWhile()
+function usingDoWhile(){
+    let i = 1;
+    do {
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+            } else if(i % 3 === 0){
+                console.log("Fizz");
+            } else if (i % 5 === 0){
+                console.log("Buzz");
+            }
+        i++
+    } while (i <= 100);
+}
+
 
 //For loop
 
@@ -45,4 +45,8 @@ function usingForLoop(){
         }
     }
 }
-usingForLoop()
+
+
+usingWhile();
+usingDoWhile();
+usingForLoop();

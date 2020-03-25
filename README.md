8098291d718d32ca3377346d7c695b29e3493810
John Martin <jrmartin182@gmail.com>
2020-03-25 11:25:44 -0600

    completed fizzbuzz assignment

---
+++ README.md
@@ -0,0 +1,24 @@
+#Assignment: FizzBuzz
+
+##Overview 
+Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz." For numbers which are multiples of both three and five print "FizzBuzz."
+
+##Learning Objectives 
+1. Loops in JavaScript
+- While
+- do...While
+- For
+2. Tech Challenge Logic
+
+##Requirements
+###Scope of Work
+
+Complete the following in all three types of Loops (While, do...While, For):
+
+1. For numbers 1 thru 100 print out the following given the condition
+- If the number is divisible by 3, print 'Fizz'
+- If the number is divisible by 5, print 'Buzz'
+- If the number is divisible by both 3 and 5, print 'FizzBuzz' 
+
+##Deliverables:
+Provide a link to your GitHub page. Answer the questions in a readme.txt file. 
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+//While loop
+// function usingWhile(){
+//     let i = 1;
+//     while(i <= 100){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz");
+//         } else if(i % 3 === 0){
+//             console.log("Fizz");
+//         } else if (i % 5 === 0){
+//             console.log("Buzz");
+//         }
+//         i++
+//     }
+// }
+// usingWhile()
+
+//Do...While loop
+// function usingDoWhile(){
+//     let i = 1;
+//     do {
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz");
+//             } else if(i % 3 === 0){
+//                 console.log("Fizz");
+//             } else if (i % 5 === 0){
+//                 console.log("Buzz");
+//             }
+//         i++
+//     } while (i <= 100);
+// }
+// usingDoWhile()
+
+//For loop
+
+function usingForLoop(){
+    for(let i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0){
+            console.log("Fizz");
+        } else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+    }
+}
+usingForLoop()
